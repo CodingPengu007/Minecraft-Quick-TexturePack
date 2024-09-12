babbc4fa21820930b502e7e8d73750b90ca46a73
Pengu <sky-pengu@outlook.com>
2024-09-12 16:14:16 +0200

    coded

--- quick-replace.py
+++ quick-replace.py
@@ -4,12 +4,14 @@ image_file = input("Enter the name of the .png image file: ")
 
 if not os.path.exists(image_file):
     print("Error: File not found")
+    input("Press Enter to exit...")
     exit()
 
 folder = input("Enter the name of the folder: ")
 
 if not os.path.exists(folder):
     print("Error: Folder not found")
+    input("Press Enter to exit...")
     exit()
 
 for root, dirs, files in os.walk(folder):
@@ -20,3 +22,6 @@ for root, dirs, files in os.walk(folder):
                 with open(image_file, 'rb') as img:
                     f.write(img.read())
             print(f"Replaced {file_path} with {image_file}")
+
+print("")
+input("Press Enter to exit...")
